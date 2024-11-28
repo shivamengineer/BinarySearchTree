ead079bbe4e1a9af6e4e3f6d303a70c990a5ef64
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-11-28 15:36:27 -0500

    implemented rotations and worked on position

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -7,6 +7,9 @@ class BinarySearchTree {
         if(this.root == null){
             node.id = 0;
             this.root = node;
+            this.root.position = 1;
+            this.root.numLeft = 0;
+            this.root.numRight = 0;
         } else {
             this.insertNode(this.root, node);
         }
@@ -17,13 +20,17 @@ class BinarySearchTree {
             if(root.right == null){
                 node.id = (root.id * 2) + 1;
                 root.right = node;
+                node.position = root.position + 1;
             } else {
                 this.insertNode(root.right, node);
             }
         } else {
+            root.position++;
+            this.increasePositionForSubtree(root.right);
             if(root.left == null){
                 node.id = root.id * 2;
                 root.left = node;
+                node.position = root.position - 1;
             } else {
                 this.insertNode(root.left, node);
             }
@@ -31,7 +38,7 @@ class BinarySearchTree {
     }
 
     remove(value){
-        this.root = this.remove(this.root, value);
+        this.root = this.removeNode(this.root, value);
     }
 
     removeNode(node, value){
@@ -39,6 +46,8 @@ class BinarySearchTree {
             return null;
         } else if(value < node.value){
             this.removeNode(node.left, value);
+            node.position--;
+            this.decreasePositionForSubtree(node.right);
             return node;
         } else if(value > node.value){
             this.removeNode(node.right, value);
@@ -74,12 +83,53 @@ class BinarySearchTree {
         }
         return minNode;
     }
+    
+    increasePositionForSubtree(node){
+        if(node != null){
+            node.position++;
+            this.increasePositionForSubtree(node.left);
+            this.increasePositionForSubtree(node.right);
+        }
+    }
 
-    rotate(){
+    decreasePositionForSubtree(node){
+        if(node != null){
+            node.position--;
+            this.decreasePositionForSubtree(node.left);
+            this.decreasePositionForSubtree(node.right);
+        }
+    }
 
+    rotateRight(node){
+        if(node.left != null){
+            var leftNode = node.left;
+            if(leftNode.right != null){
+                node.left = leftNode.right;
+            }
+            leftNode.right = node;
+        }
+    }
+
+    rotateLeft(node){
+        if(node.right != null){
+            var rightNode = node.right;
+            if(rightNode.left != null){
+                node.right = rightNode.left;
+            }
+            rightNode.left = node;
+        }
     }
 
     draw(){
-        
+        if(this != null)
+            drawNode(this.root);
+    }
+
+    drawNode(node){
+        if(node != null){
+            //draw
+            this.drawNode(node.left);
+            this.drawNode(node.right);
+        }
     }
 }
